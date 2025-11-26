e20af6561673011e53372fa8ba292c621921d04d
Daniel <crj0035769@est.univalle.edu>
2025-11-25 21:38:55 -0400

    Primer commit - Proyecto ASP.NET MVC ArreglaMiCiudad

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArreglaMiCiudad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
